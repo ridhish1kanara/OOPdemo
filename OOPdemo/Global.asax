a87f84e6563c6059b3872bbd2cea046aee66449d
Ridhish Kanara <200440526@student.georgianc.on.ca>
2020-10-07 21:51:19 -0700

    Add project files.

---
+++ OOPdemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OOPdemo.MvcApplication" Language="C#" %>
---
+++ OOPdemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OOPdemo.Startup))]
+namespace OOPdemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
